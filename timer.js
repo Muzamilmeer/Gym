7b66bd8f6586fcf4bd91921eb087667c17d5199d
Muzamil Ahmad Mir <127821271+Muzamilmeer@users.noreply.github.com>
2025-07-12 14:43:12 +0530

    Create timer.js

---
+++ timer.js
@@ -0,0 +1,20 @@
+// Set countdown 7 days from now
+var countDownDate = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000).getTime();
+
+var x = setInterval(function () {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("demo").innerHTML =
+    days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
